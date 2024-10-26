80452698a93e1ea8e1779b69d32301f6a358b104
Yash rai <yash2154rai@gmail.com>
2024-10-26 17:57:21 +0530

    remove test lines

--- arraylist.py
+++ arraylist.py
@@ -63,11 +63,3 @@ class ArrayList:
         if not self.head:
             raise KeyError
         return self.map[key].data
-
-list1 = ArrayList()
-list1.append(1)
-list1.append(2)
-list1.append(3)
-list1.append(4)
-print(list1)
-print(list1[3])

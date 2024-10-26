ceffcd4bfb9e79d9ad75c3e0c549c6491e82a065
Yash rai <yash2154rai@gmail.com>
2024-10-26 17:56:38 +0530

    first commit

---
+++ arraylist.py
@@ -0,0 +1,73 @@
+from typing import Any
+
+class ArrayNode:
+    def __init__(self,data = None,nextNode = None):
+        self.data = data
+        self.nextNode = nextNode
+
+class ArrayList:
+    def __init__(self,head = None,tail = None):
+        self.head = head
+        self.tail = tail
+        self.map = dict()
+        self.counter = 0
+    def append(self,data:Any):
+        newNode = ArrayNode(data)
+        if not self.head:
+            self.tail = newNode
+            self.head = newNode
+            self.map[self.counter] = newNode
+            self.counter += 1
+        else:
+            self.tail.nextNode = newNode
+            self.tail = newNode
+            self.map[self.counter] = newNode
+            self.counter += 1
+    def display(self):
+        current = self.head
+        while current:
+            print()
+    def __len__(self):
+        return self.counter
+    
+    def __str__(self):
+        string = "["
+        current = self.head
+        while current:
+            if  not current.nextNode:
+                string = string + str(current.data)
+            else:
+                string = string + str(current.data) + ", "
+            current = current.nextNode
+        string += "]"
+        return string
+    def __add__(self,otherList):
+        otherList:ArrayList
+        current:ArrayNode = otherList.head
+        while current:
+            self.append(current.data)
+            current = current.nextNode
+        return self
+    
+    def index(self,value):
+        idx = 0
+        current = self.head
+        while current:
+            if value == current.data:
+                return idx
+            idx +=1
+            current = current.nextNode
+        
+        return -1
+    def __getitem__(self,key):
+        if not self.head:
+            raise KeyError
+        return self.map[key].data
+
+list1 = ArrayList()
+list1.append(1)
+list1.append(2)
+list1.append(3)
+list1.append(4)
+print(list1)
+print(list1[3])
---
+++ test.py
@@ -0,0 +1,10 @@
+from arraylist import ArrayList
+
+
+list1 = ArrayList()
+list1.append(1)
+list1.append(2)
+list1.append(3)
+list1.append(4)
+print(list1)
+print(list1[3])
